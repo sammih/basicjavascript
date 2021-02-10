e958fb5281ee5c0ce3ff80978bb8e6333c7f5a60
sammih <samcessy@gmail.com>
2021-02-10 11:02:11 +0300

    adds first commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# This tutorial is meant for beginners of Javascript.
+
+### Functions
+**TODO**: Definitions
+**TODO**: Declaration
+**TODO**: Javascript Function Syntax
+**TODO**: Example One:
---
+++ js/app.js
@@ -0,0 +1,17 @@
+let sum = function(num1, num2) {
+    return num1 + num2;
+}
+
+let exampleOne = document.querySelector('.example-one');
+exampleOne.innerHTML = sum(8,4);
+
+let codeToggler = document.querySelector('.fa-chevron-down');
+codeToggler.addEventListener('click', function () {
+    let code = document.querySelector(".code");
+
+    if (code.style.display === "block")
+    code.style.display = "none";
+    else {
+        code.style.display = "block";
+    }
+});
